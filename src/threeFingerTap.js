93c39ec7ee32805cd7eb50d27b7cbf76bf4b3c18
sidthesloth <www.sidthesloth@gmail.com>
2016-06-30 09:37:00 +0530

    feat(browser): Added init function
    Get the elements with '.three_finger_tap' and store it in nodes

--- src/threeFingerTap.js
+++ src/threeFingerTap.js
@@ -1,3 +1,15 @@
 ((window) => {
-    window.threeFingerTap  = {};
+    var threeFingerTap  = {};
+
+    threeFingerTap.nodes = [];
+
+
+    threeFingerTap.init = init;
+
+    function init() {
+        var nodes = document.querySelectorAll('.three-finger-tap');
+        threeFingerTap.nodes = Array.from(nodes);
+    }
+
+    window.threeFingerTap = threeFingerTap;
 })(window);
