04ccd59011aa8395bf6eb22f72669cbe26f1c06e
sidthesloth92 <www.sidthesloth@gmail.com>
2017-06-14 19:45:34 +0530

    test(test): added new test cases for init

--- tests/init.test.js
+++ tests/init.test.js
@@ -1,14 +1,43 @@
 const threeFingerTap = require('../dist/js/threeFingerTap.js');
 
+let name = 'three-finger-tap';
+let hoverTimeout = 1000;
 
 describe("test initialization", () => {
     
-    test("test for name" , () => {
+    test("test name initialization and hover default" , () => {
         threeFingerTap.init({
-            name : 'three-finger-tap',
-            hoverTimeout : 1000
+            name : name,
+            hoverTimeout : hoverTimeout
         });
-        expect(threeFingerTap.getName()).toBe('three-finger-tap');
+
+        expect(document.body.getElementsByClassName('tft_iframe_wrapper')).toHaveLength(1);
+        expect(threeFingerTap.getName()).toBe(name);
+        expect(threeFingerTap.getHoverTimeout()).toEqual(hoverTimeout);
+    });
+
+    test("test custom hoverTimeout initialization" , () => {
+        threeFingerTap.init({
+            name : name,
+            hoverTimeout : 2000
+        });
+
+        expect(document.body.getElementsByClassName('tft_iframe_wrapper')).toHaveLength(1);
+        expect(threeFingerTap.getHoverTimeout()).toEqual(2000);
+    });
+
+    test("test default customLoadingBackground" , () => {
+        threeFingerTap.init({
+            name : name,
+            hoverTimeout : hoverTimeout
+        });
+        
+        expect(document.body.getElementsByClassName('tft_iframe_wrapper')).toHaveLength(1);
+        expect(document.body.querySelector('.tft_iframe_wrapper').querySelector('.loader')).toBeDefined();
+    });
+    
+    afterEach(() =>  {
+        threeFingerTap.destroy();
     });
 
 });
